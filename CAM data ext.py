2cdc4fa4c0b0406c0f40426478b507ac6dbed9f9
Hemal-Vadaliya <109291639+Hemal-Vadaliya@users.noreply.github.com>
2023-09-22 13:02:03 +0530

    Update CAM data ext.py

--- CAM data ext.py
+++ CAM data ext.py
@@ -27,9 +27,6 @@ except KeyboardInterrupt:
     print("Serial connection closed.")
 #print(len(dt))
 
-
-dt = [1, 3, 2, 1, 2, 2, 0, 0, 0, 0, 3, 1, 2, 0, 0, 4, 2, 2, 2, 0, 5, 0, 0, 0, 0, 6, 1, 2, 0, 0, 7, 1, 1, 0, 0, 8, 1, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0]
-
 cm_dt = dt[40:]
 #print(cm_dt)
 dt_dict = {
